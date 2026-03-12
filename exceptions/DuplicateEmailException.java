a54152c2634042aeed1a1721421768fad9e65457
ZahiraAtmani <zahira.a28@hotmail.fr>
2026-03-12 07:26:17 -0400

    Add files via upload
    adding the exceptions files

---
+++ exceptions/DuplicateEmailException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class DuplicateEmailException extends RuntimeException {
+	
+	public DuplicateEmailException(String message) {
+		super(message);
+	}
+	
+
+}
---
+++ exceptions/EntityNotFoundException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class EntityNotFoundException extends Exception {
+	
+	public EntityNotFoundException(String message) {
+		super(message);
+	}
+	
+
+}
