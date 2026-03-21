bc1311aef56c7abdf4e2e75849e7d03f07aad68d
ZahiraAtmani <zahira.a28@hotmail.fr>
2026-03-20 21:25:32 -0400

    Add comment for InvalidTransportDataException
    Added a comment explaining the purpose of the custom exception.

--- exceptions/InvalidTransportDataException.java
+++ exceptions/InvalidTransportDataException.java
@@ -4,10 +4,14 @@
 // 		Wania Faraz 40332781
 //		Zahira Atmani 40350242
 //-----------------------------------------------------------------------------
+// this custom exception is used when invalid data is encountered while creating or loading transportation object.
+//
+//
+//
 package exceptions;
 
 public class InvalidTransportDataException extends Exception {
-	
+	//create a new exception with a specific message describing the error
 	public InvalidTransportDataException(String message) {
 		super(message);
 	}
