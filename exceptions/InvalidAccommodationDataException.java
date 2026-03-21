cf7d73de41b9cd52b471e2ca5d795f4e6872b9a1
ZahiraAtmani <zahira.a28@hotmail.fr>
2026-03-20 21:23:26 -0400

    Enhance comments in InvalidAccommodationDataException
    Added comments to clarify the purpose of the exception class.

--- exceptions/InvalidAccommodationDataException.java
+++ exceptions/InvalidAccommodationDataException.java
@@ -4,10 +4,14 @@
 // 		Wania Faraz 40332781
 //		Zahira Atmani 40350242
 //-----------------------------------------------------------------------------
+// this custom exception is used when invalid data is encountered when creating or loading
+// accommodation object.
+//
+//
 package exceptions;
 
 public class InvalidAccommodationDataException extends Exception{
-	
+	//create a new exception with a specific message describing the error
 	public InvalidAccommodationDataException(String message) {
 		super(message);
 	}
