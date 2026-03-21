fccb1e340c134c687f23dd3da9dc85f1fc481352
ZahiraAtmani <zahira.a28@hotmail.fr>
2026-03-20 21:15:44 -0400

    Enhance DuplicateEmailException with descriptive comment
    Add a comment to describe the purpose of the exception.

--- exceptions/DuplicateEmailException.java
+++ exceptions/DuplicateEmailException.java
@@ -4,10 +4,14 @@
 // 		Wania Faraz 40332781
 //		Zahira Atmani 40350242
 //-----------------------------------------------------------------------------
+// This custom exception is used to indicate that an attempt was made to create or load a client 
+// with an email that already exists in the system.
+//
+//
 package exceptions;
 
 public class DuplicateEmailException extends RuntimeException {
-	
+	//create a new exception with a specific message that describes the error
 	public DuplicateEmailException(String message) {
 		super(message);
 	}
