5396f3d03c3e390075b55f624355e9b8cc1a25f0
ZahiraAtmani <zahira.a28@hotmail.fr>
2026-03-20 21:22:18 -0400

    Clarify purpose of EntityNotFoundException
    Added comment to clarify the purpose of the custom exception.

--- exceptions/EntityNotFoundException.java
+++ exceptions/EntityNotFoundException.java
@@ -4,10 +4,14 @@
 // 		Wania Faraz 40332781
 //		Zahira Atmani 40350242
 //-----------------------------------------------------------------------------
+// this custom exception is used when an entity (client, accommodation, transportation, trip)cannot be found.
+//
+//
+//
 package exceptions;
 
 public class EntityNotFoundException extends Exception {
-	
+	//create a new exception with a specific message describing which entity was not found
 	public EntityNotFoundException(String message) {
 		super(message);
 	}
