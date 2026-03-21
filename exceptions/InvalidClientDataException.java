e1959a0a5b01ee910176ca969da657f5516d7452
ZahiraAtmani <zahira.a28@hotmail.fr>
2026-03-20 21:24:21 -0400

    Add purpose comment to InvalidClientDataException
    Add comment to describe the purpose of the exception.

--- exceptions/InvalidClientDataException.java
+++ exceptions/InvalidClientDataException.java
@@ -4,10 +4,14 @@
 // 		Wania Faraz 40332781
 //		Zahira Atmani 40350242
 //-----------------------------------------------------------------------------
+// this custom exception is used when invalid data is provided while creating or loading client object.
+//
+//
+//
 package exceptions;
 
 public class InvalidClientDataException extends Exception {
-	
+	//create a new exception with a specific message describing the error
 	public InvalidClientDataException(String message) {
 		super(message);
 	}
