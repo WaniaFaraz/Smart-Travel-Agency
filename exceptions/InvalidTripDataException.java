70138ed81ec556c05f85465cb5b68c246cae781b
ZahiraAtmani <zahira.a28@hotmail.fr>
2026-03-20 21:27:22 -0400

    Add purpose comment to InvalidTripDataException
    Add comment explaining the purpose of the exception.

--- exceptions/InvalidTripDataException.java
+++ exceptions/InvalidTripDataException.java
@@ -4,10 +4,14 @@
 // 		Wania Faraz 40332781
 //		Zahira Atmani 40350242
 //-----------------------------------------------------------------------------
+// this custom exception is used when invalid data is encountered while creating or loading a trip object.
+//
+//
+//
 package exceptions;
 
 public class InvalidTripDataException extends Exception {
-	
+	//create new exception with a specific message describing the error
 	public InvalidTripDataException(String message) {
 		super(message);
 	}
